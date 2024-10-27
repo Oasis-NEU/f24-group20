aca297dacf5c083f63de2e53b0ef3666adf99811
Ameeka-Patel <patel.ameek@northeastern.edu>
2024-10-27 13:16:25 -0400

    formatted info for page 2

--- src/assets/Courses.jsx
+++ src/assets/Courses.jsx
@@ -3,9 +3,22 @@ import Navigation from "./navigation";
 
 const Courses = () => {
     return(
-    <div>
-    <Navigation/>
-    </div>
+        <>
+         <div>
+         <Navigation/>
+         <h1 className="profName">Professor Name</h1>
+         <h2 className="profEmail"> Email</h2>
+         <h2 className="linkedIn">LinkedIn</h2>
+         <h2 chassName= "className">  RateMyProfessor </h2>
+
+         <h1 className="className">Course Name</h1>
+         <h1 className="location">Class Location</h1>
+         <h2 className="classMap"> Classroom Map </h2>
+         <h2 className="classPic"> Classroom Picture</h2>
+        </div>
+        
+        </>
+   
     );
 }
 export default Courses
