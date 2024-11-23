f76f75a99f0620fcaf12d007262c44ac2d3d8aed
Ameeka-Patel <patel.ameek@northeastern.edu>
2024-11-22 22:16:56 -0500

    meeting times

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -36,6 +36,7 @@ function Courses() {
           </div>
           <h2 className="profEmail">Email: {data[3]}</h2>
           <h2 chassName="className">Rating: {data[11]}</h2>
+          <h2 chassName="meetingTimes">Meeting Time: </h2>
           <h1 className="location">Room: <br></br>{data[1]}</h1>
           <div className="mapFormat">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24207.383434676187!2d-71.12799802568361!3d42.33990399999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e37a1999cf5ce1%3A0xc97b00e66522b98c!2sNortheastern%20University!5e1!3m2!1sen!2sus!4v1731866750161!5m2!1sen!2sus" width="400" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
